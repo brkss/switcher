6c97bb8beadddd4bc81090e45a6b751a5ff09269
Brahim Berkasse <berkassebrahim@gmail.com>
2022-01-28 21:38:17 +0100

    🦚 List sql files in directory !

--- dist/utils/save.js
+++ dist/utils/save.js
@@ -4,11 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.save = void 0;
+const fs_1 = __importDefault(require("fs"));
 const db_json_1 = __importDefault(require("./db.json"));
+const path_1 = __importDefault(require("path"));
 const save = async () => {
+    fs_1.default.readdir(path_1.default.join(__dirname, "databases"), (err, files) => {
+        console.log("error listing files => ", err);
+        console.log("list dir data => ", files);
+    });
     const data = db_json_1.default;
     data.map((database) => { });
-    console.log("db => ", db_json_1.default);
+    console.log("dbs => ", db_json_1.default);
 };
 exports.save = save;
 //# sourceMappingURL=save.js.map
--- src/utils/save.ts
+++ src/utils/save.ts
@@ -1,6 +1,7 @@
-import exec from "child_process";
+import { spawn } from "child_process";
 import fs from "fs";
 import db from "./db.json";
+import path from "path";
 
 interface IDatabase {
   name: string;
@@ -9,7 +10,20 @@ interface IDatabase {
 }
 
 export const save = async () => {
+  /*
+  const ls = spawn("ls", ["-lh", "/databases"]);
+  ls.stdout.on("data", (data) => {
+    console.log("ls data -> ", data);
+  });
+  ls.stderr.on("data", (e) => {
+    console.log("err => ", e);
+  });
+  */
+  fs.readdir(path.join(__dirname, "databases"), (err, files) => {
+    console.log("error listing files => ", err);
+    console.log("list dir data => ", files);
+  });
   const data: IDatabase[] = db;
   data.map((database) => {});
-  console.log("db => ", db);
+  console.log("dbs => ", db);
 };
